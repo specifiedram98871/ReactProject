620d12322f33c59d74b9b1ececdaec8e92326ca2
specifiedram98871 <ramkm98871@gmail.com>
2024-02-23 06:37:47 +0545

    working array

---
+++ simple/src/components/color.jsx
@@ -0,0 +1,5 @@
+export default function Color({ value }) {
+    return(
+       <div className="w-32 h-32 m-10" style={{backgroundColor: value}} />
+    )
+}
--- simple/src/components/inspiration.jsx
+++ simple/src/components/inspiration.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Fancy from './fancy'
 import { useState } from 'react'
 import quotes from './quote'
-
+import Color from './color'
 const Inspiration = ({ children }) => {
     const [index, setIndex] = useState(0);
     const quote = quotes[index];
@@ -11,9 +11,12 @@ const Inspiration = ({ children }) => {
     }
   return (
       <div className='flex flex-col justify-center  bg-slate-400 '>
-          <p className='text-2xl'>Your inspirational quote is:</p>
-          <Fancy text={quote} />
-          <button className='py-2 px-5 bg-violet-500 text-white font-semibold rounded-full ' onClick={next}>Click for quote</button>
+          <p className='text-2xl'>Your inspirational {quote.type} is:</p>
+      {quote.type === 'quote' ?
+        <Fancy text={quote.value} /> :
+        <Color value={quote.value} />
+      }
+          <button className='py-2 px-5 bg-violet-500 text-white font-semibold rounded-full' onClick={next}>Click for quote</button>
           {children}
     </div>
   )
