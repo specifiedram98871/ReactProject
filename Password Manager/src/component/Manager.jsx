977b14e3580e2c15aef4e2b095f298cf91a6ea43
specifiedram98871 <ramkm98871@gmail.com>
2024-03-24 20:37:04 +0545

    Ui

--- Password Manager/src/component/Manager.jsx
+++ Password Manager/src/component/Manager.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React from "react";
 
 const Manager = () => {
   return (
-      <div className='mycontainer bg-slate-600 w-full' >
-      <div className="logo font-bold text-center p-2">
-                  <span className="text-sky-600">&lt;</span>
-                   Pass   <span className='text-sky-600'>Mg</span>
-                  <span className="text-sky-600"> /&gt;</span>
-      <p>Password Manager</p>
-              </div>
-          <div className='text-white flex flex-col'>
-              <input className='rounded-full' type="text" name='' id='' />
-              <div className="flex justify-between py-4">
-                  <input className='rounded-full w-full mr-3' type="text" name='' id=''/>
-                  <input className='rounded-full' type="password" name='' id=''/>
-              </div>
-          </div>
-    </div>
-  )
-}
+    <div className="mycontainer bg-slate-600 w-full">
+      <div className="logo text-center p-2">
+        <h1 className="text-4xl font-bold">
+          {" "}
+          <span className="text-sky-600">&lt;</span>
+          Pass <span className="text-sky-600">Mg</span>
+          <span className="text-sky-600"> /&gt;</span>
+        </h1>
+        <p className="text-2xl text-sky-500">Password Manager</p>
+      </div>
+      <div className="text-black flex p-4 flex-col items-center gap-8">
+        <input
+          className="rounded-full border border-sky-500 w-full p-4 py-1"
+          type="text"
+          name=""
+          id=""
+        />
+        <div className="flex w-full justify-between gap-9">
+          <input
+            className="rounded-full border border-sky-500 w-full p-4 py-1"
+            type="text"
+            name=""
+            id=""
+          />
+          <input
+            className="rounded-full border border-sky-500 w-full p-4 py-1"
+            type="text"
+            name=""
+            id=""
+          />
+        </div>
 
-export default Manager
+        <button className="flex items-center bg-sky-600  rounded-full w-fit px-4 hover:bg-sky-400">
+          <lord-icon
+            src="https://cdn.lordicon.com/zrkkrrpl.json"
+            trigger="in"
+            style={{ width: "40px", height: "40px", color: "white" }}
+          ></lord-icon>
+          Add password
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Manager;
