29207bf11ec685a51454e3e2a63a4dd72eaacb50
specifiedram98871 <ramkm98871@gmail.com>
2024-03-25 11:56:44 +0545

    UI completion

--- Password Manager/src/component/Manager.jsx
+++ Password Manager/src/component/Manager.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Manager = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <div className="mycontainer bg-slate-600 w-full">
       <div className="logo text-center p-2">
@@ -26,12 +32,35 @@ const Manager = () => {
             name=""
             id=""
           />
-          <input
-            className="rounded-full border border-sky-500 w-full p-4 py-1"
-            type="text"
-            name=""
-            id=""
-          />
+          <div className="relative">
+            <input
+              className="rounded-full border border-sky-500 w-full p-4 py-1"
+              type={showPassword ? "text" : "password"}
+              name=""
+              id=""
+            />
+            <span
+              className="absolute right-0 top-1 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <lord-icon
+                  src="https://cdn.lordicon.com/vfczflna.json"
+                  trigger="in"
+                  delay="1500"
+                  style={{ width: "30px", height: "30px" }}
+                ></lord-icon>
+              ) : (
+                <lord-icon
+                  src="https://cdn.lordicon.com/vfczflna.json"
+                  trigger="in"
+                  delay="1500"
+                  state="hover-cross"
+                  style={{ width: "30px", height: "30px" }}
+                ></lord-icon>
+              )}
+            </span>
+          </div>
         </div>
 
         <button className="flex items-center bg-sky-600  rounded-full w-fit px-4 hover:bg-sky-400">
