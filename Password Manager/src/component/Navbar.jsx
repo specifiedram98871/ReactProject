49eb0a8d07a22b03d540f922f17c66bbd7cdb243
specifiedram98871 <ramkm98871@gmail.com>
2024-03-28 17:56:42 +0545

    action button

--- Password Manager/src/component/Navbar.jsx
+++ Password Manager/src/component/Navbar.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (  
-      <div className='bg-slate-800 '>
-          <div className="mycontainer flex justify-between px-4 py-5 h-14 items-center">
-              <div className="logo font-bold">
-                  <span className="text-sky-600">&lt;</span>
-                   Pass   <span className='text-sky-600'>Mg</span>
-                  <span className="text-sky-600"> /&gt;</span>
-              </div>
-          <ul className="flex gap-4 text-white items-center">
-              <li className='hover:bg-sky-600 p-4'>Home</li>
-              <li className='hover:bg-sky-600 p-4'>Contact</li>
-              <li className='hover:bg-sky-600 p-4'>About Me</li>
-              <li className='hover:bg-sky-600 p-4'>Download</li>
-          </ul>
-          <div className='bg-sky-600 text-white border-2px border-sky-600 rounded-full p-1'>
-                  <img src="" alt="" /> 
-        <span>GitHub</span>          
-          </div>
-          </div>
+  return (
+    <div className="bg-slate-800 ">
+      <div className="mycontainer flex justify-between px-4 py-5 h-14 items-center">
+        <div className="logo font-bold">
+          <span className="text-sky-600">&lt;</span>
+          Pass <span className="text-sky-600">Mg</span>
+          <span className="text-sky-600"> /&gt;</span>
+        </div>
+        <ul className="flex gap-4 text-white items-center">
+          <li className="hover:bg-sky-600 p-4">Home</li>
+          <li className="hover:bg-sky-600 p-4">Contact</li>
+          <li className="hover:bg-sky-600 p-4">About Me</li>
+          <li className="hover:bg-sky-600 p-4">Download</li>
+        </ul>
+        <div className="bg-sky-600 text-white border-2px border-sky-600 rounded-full p-1">
+          <img src="" alt="" />
+          <span>GitHub</span>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Navbar;
-
-
-
--- Password Manager/src/component/table.jsx
+++ Password Manager/src/component/table.jsx
@@ -2,30 +2,47 @@ import React from "react";
 
 const Table = ({ passwords }) => {
   return (
-    <div className="mycontainer overflow-x-auto">
-      <h1 className="text-3xl font-bold text-center my-8">
-        Your Saved Passwords
-      </h1>
-      <table className="table-auto w-full border-collapse">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="px-4 py-2">Site</th>
-            <th className="px-4 py-2">Username</th>
-            <th className="px-4 py-2">Password</th>
-          </tr>
-        </thead>
-        <tbody>
-          {passwords.map((p, index) => {
-            return (
-              <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : ""}>
-                <td className="border px-4 py-2">{p.site}</td>
-                <td className="border px-4 py-2">{p.username}</td>
-                <td className="border px-4 py-2">{p.password}</td>
+    <div>
+      {passwords.length === 0 ? (
+        <h1 className="text-2xl text-sky-400 font-bold text-start m-8">
+          No Passwords Found
+        </h1>
+      ) : (
+        <div className="mycontainer overflow-x-auto">
+          <h1 className="text-2xl font-bold text-start my-8">
+            Your Saved Passwords
+          </h1>
+          <table className="table-auto w-full border-collapse">
+            <thead>
+              <tr className="bg-sky-300">
+                <th className="px-4 py-2">Site</th>
+                <th className="px-4 py-2">Username</th>
+                <th className="px-4 py-2">Password</th>
+                <th className="px-4 py-2">Action</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {passwords.map((p, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">{p.site}</td>
+                    <td className="border px-4 py-2">{p.username}</td>
+                    <td className="border px-4 py-2">{p.password}</td>
+                    <div className=" border flex justify-around p-1">
+                      <button className="bg-sky-600 text-white px-4 py-2 rounded-lg">
+                        Edit
+                      </button>
+                      <button className="bg-red-600 text-white px-4 py-2 rounded-lg">
+                        Delete
+                      </button>
+                    </div>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
