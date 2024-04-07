1b3eb83aa2532874700d3943e2e416ed8b0bad42
specifiedram98871 <ramkm98871@gmail.com>
2024-04-07 19:05:10 +0545

    store in mongoose

--- Backend/server.js
+++ Backend/server.js
@@ -12,20 +12,20 @@ const url = process.env.MONGO_DB_URL;
 
 // Define Mongoose Schema
 const passopSchema = new mongoose.Schema({
+  id: Number,
   name: String,
+  age: Number,
 });
 
 // Create Mongoose Model for the "passop" collection
 const Passop = mongoose.model("passop", passopSchema); // Third argument specifies the collection name
 
-// const newData = new Passop({ name: "Ram" });
-// newData.save();
 // Establish MongoDB Connection
 async function connectToDatabase() {
   try {
     await mongoose.connect(url, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
   } catch (error) {
@@ -37,25 +37,26 @@ connectToDatabase();
 // Define Route Handlers
 app.post("/insert", async (req, res) => {
   try {
-    const newData = {
-      name: "Ram",
-    };
-    const data = await Passop.create(newData);
+    const newDocuments = [
+      { name: "Ram", age: 21, id: 4 },
+      { name: "Ramm", age: 22, id: 7 },
+    ];
+    const data = Passop.insertMany(newDocuments); // Wait for the data insertion
     console.log("Data inserted successfully");
     res.json(data);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 app.get("/", async (req, res) => {
   try {
-    const data = await Passop.find();
+    const data = await Passop.find({});
     res.json(data);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
